82041aca11ebbb76814ab4ff21eb2e866881b398
DeveloperIgorR <bratca@inbox.ru>
2021-10-28 23:51:15 +0300

    mongoose schema add

---
+++ Students.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Students = new mongoose.Schema({
+    name:{type: String, required: true},
+    Telegram:{type: String, required: true},
+    Instagram:{type: String, required: true},
+    date:{type: String, required: true},
+    login:{type: String, required: true},
+    module:{type: Object, required: true}
+})
+
+
+module.exports = mongoose.model('Students',Students)
