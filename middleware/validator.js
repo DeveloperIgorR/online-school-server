b18e32b6fb0391d5ce2850d844092954c59b5d1e
DeveloperIgorR <bratca@inbox.ru>
2021-11-07 23:40:43 +0200

    add validator

---
+++ middleware/validator.js
@@ -0,0 +1,41 @@
+const { body } = require("express-validator");
+
+class Validator {
+  validateLogin(request) {
+    return [
+      body("email")
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("Логин должен быть e-mail"),
+      body("password")
+        .isStrongPassword({
+          minLength: 3,
+        //   minLowercase: 1,
+          minNumbers: 1,
+        //   minUppercase: 1,
+        //   minSymbols: 1,
+        })
+        .withMessage(
+          "Пароль должен содержать минимум 3 символа"
+        ),
+    ];
+  }
+  validateSN(requers) {
+    return [
+      body("Instagram")
+        .isLength({ min: 2, max: 16 })
+        .matches(
+          /@(?=.{5,64}(?:\s|$))(?![_])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_.])/gm
+        )
+        .withMessage("Введите корректный профиль Instagram, начинающийся с @"),
+      body("Telegram")
+        .isLength({ min: 2, max: 16 })
+        .matches(
+          /@(?=.{5,64}(?:\s|$))(?![_])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_.])/gm
+        )
+        .withMessage("Введите корректный профиль Telegram, начинающийся с @"),
+    ];
+  }
+}
+
+module.exports = new Validator();
