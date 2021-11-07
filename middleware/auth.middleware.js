5ef0a80a443b9356b7d2d5960cf211022fea8553
DeveloperIgorR <bratca@inbox.ru>
2021-11-07 22:22:08 +0200

    auth middleware add

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+module.exports = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+       return next()
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({message: 'Auth error'})
+        }
+        const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        req.user = decoded
+        next()
+    } catch (e) {
+        return res.status(401).json({message: 'Auth error'})
+    }
+}
