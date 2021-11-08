1dc180e46c87d6bce78745557d18a728a26ee431
DeveloperIgorR <bratca@inbox.ru>
2021-11-08 20:52:40 +0200

    validation fix

--- services/users.services.js
+++ services/users.services.js
@@ -2,27 +2,26 @@ const Users = require('../Users')
 const bcrypt = require("bcrypt")
 const salt = 5
 
-class UsersService{
+class UsersService {
 
-    async registration(body) {
-        try {
-          let { email, password } = body
-          const hashedPassword = await bcrypt.hash(password, salt)
-          return new Promise(async (res, rej) => {
-            let candidate = await Users.findOne({ where: { email } })
-            if (candidate) {
-              rej("User with this login already exists")
-            } else {
-              console.log('$$$$')
-              const newUser = Users.create({ ...body, password: hashedPassword })
-              console.log(newUser)
-              res(newUser)
-            }
-          });
-        } catch {
-          if (err) throw err
+  async registration(body) {
+    try {
+      let { email, password } = body
+      const hashedPassword = await bcrypt.hash(password, salt)
+      return new Promise(async (res, rej) => {
+        let candidate = await Users.findOne({ email: email })        
+        if (candidate) {
+          rej("User with this login already exists")
+        } else {          
+          const newUser = await Users.create({ ...body, password: hashedPassword })
+          console.log(newUser)
+          res(newUser)
         }
-      }
+      });
+    } catch (e) {      
+      res.send({ message: "Server error" })
+    }
+  }
 }
 
 module.exports = new UsersService
