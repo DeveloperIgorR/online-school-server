44082bb2f06f17135b9014addca7a6437c90ffc4
DeveloperIgorR <bratca@inbox.ru>
2021-12-08 23:09:41 +0200

    auth

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS') {
        return next()
     }
-
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
